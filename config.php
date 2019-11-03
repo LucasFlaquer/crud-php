7ba91f0d2088038c7bf8dd00e213c10e30fd1e8a
lucasflaquer <lucas.flaquer@gmail.com>
2019-11-03 15:59:14 -0300

    etapa 2 do tutorial concluida

--- config.php
+++ config.php
@@ -17,8 +17,12 @@
   
   /** caminho no server para o sistema **/	
     if ( !defined('BASEURL') )
-      define('BASEURL', 'localhost');
+      define('BASEURL', '');
   
   /** caminho do arquivo de banco de dados **/
     if ( !defined('DBAPI') )
       define('DBAPI', ABSPATH . 'inc/database.php');
+
+  define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');	
+  define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
+?>
--- index.php
+++ index.php
@@ -1,35 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <title>Document</title>
-</head>
-<body>
-  <?php require_once 'config.php';?>
-  <?php require_once DBAPI;?>
-  <div class="container">
-    <?php
-      $db = open_database();
-      if($db) {
-        echo '<h1>Banco de Dados conectado!!!</h1>';
-      } else {
-        echo 'ERRO: nao foi póissive conectar';
-      }
+<?php require_once 'config.php'?>
+<?php require_once DBAPI; ?>
 
-
-      close_database($db);
-    ?>
-  </div>
-
-
-
-
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include(HEADER_TEMPLATE); ?>
+<?php $db = open_database(); ?>	
+<h1>Dashboard</h1>
+<hr />	
+<?php if ($db) : ?>	
+  <div class="row">
+    <div class="col-xs-6 col-sm-3 col-md-2">
+      <a href="customers/add.php" class="btn btn-primary">
+        <div class="row">
+          <div class="col-xs-12 text-center"><i class="fa fa-plus fa-5x"></i></div>
+          <div class="col-xs-12 text-center"><p>Novo Cliente</p></div>	
+        </div>	
+      </a>	
+    </div>	
+    <div class="col-xs-6 col-sm-3 col-md-2">
+      <a href="customers" class="btn btn-default">	
+        <div class="row">			
+          <div class="col-xs-12 text-center"><i class="fa fa-user fa-5x"></i></div>
+          <div class="col-xs-12 text-center"><p>Clientes</p></div>
+        </div>		
+      </a>	
+    </div>
+  </div>	
+<?php else : ?>		
+  <div class="alert alert-danger" role="alert">
+    <p><strong>ERRO:</strong> Não foi possível Conectar ao Banco de Dados!</p>		
+  </div>	
+<?php endif; ?>	
+<?php include(FOOTER_TEMPLATE); ?>
